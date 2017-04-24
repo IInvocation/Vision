c0d5b5813a514e31cb89842f6622931a3acb854c
Achim Schnell <a.schnell@wtschnell.de>
2017-04-24 14:19:06 +0200

    Readded ConsoleApp1 (serves as testing-project)

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        Console.WriteLine("Please enter your username");
+	        var usr = Console.ReadLine();
+			Console.WriteLine("Please enter your password");
+	        var pwd = Console.ReadLine();
+			var response = TestCredentials(usr, pwd);
+			Console.WriteLine(response.Result.HttpStatusCode == HttpStatusCode.OK);
+	        Console.ReadLine();
+        }
+
+	    static async Task<TokenResponse> TestCredentials(string user, string pwd)
+	    {
+			var disco = await DiscoveryClient.GetAsync("http://localhost:1234");
+		    var tokenClient = new TokenClient(disco.TokenEndpoint, "Jarvis", "Test");
+		    return await tokenClient.RequestResourceOwnerPasswordAsync("a.schnell@wtschnell.de", pwd, "api1");
+	    }
+    }
+}
