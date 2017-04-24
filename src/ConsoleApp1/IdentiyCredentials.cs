2681e883cd1270a03ee5cfae17a0a47dc4c99076
SAPPHIRE\aschnell <a.schnell@wtschnell.de>
2017-04-24 17:54:54 +0200

    Cleaned up the CredentialsTestApp to be less annoying

---
+++ src/ConsoleApp1/IdentiyCredentials.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+	/// <summary>	An identiy credentials. </summary>
+	public class IdentiyCredentials
+	{
+		/// <summary>	Gets or sets the name of the user. </summary>
+		/// <value>	The name of the user. </value>
+		public string UserName { get; set; }
+
+		/// <summary>	Gets or sets the password. </summary>
+		/// <value>	The password. </value>
+		public string Password { get; set; }
+
+		/// <summary>	Gets or sets the identifier of the client. </summary>
+		/// <value>	The identifier of the client. </value>
+		public string ClientId { get; set; }
+
+		/// <summary>	Gets or sets the client secret. </summary>
+		/// <value>	The client secret. </value>
+		public string ClientSecret { get; set; }
+	}
+}
--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,28 +1,46 @@
 ﻿using System;
 using System.Net;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using IdentityModel.Client;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApp1
 {
-    class Program
+	/// <summary>	A program. </summary>
+	class Program
     {
+        /// <summary>	Main entry-point for this application. </summary>
+        /// <param name="args">	An array of command-line argument strings. </param>
         static void Main(string[] args)
         {
-	        Console.WriteLine("Please enter your username");
-	        var usr = Console.ReadLine();
-			Console.WriteLine("Please enter your password");
-	        var pwd = Console.ReadLine();
-			var response = TestCredentials(usr, pwd);
-			Console.WriteLine(response.Result.HttpStatusCode == HttpStatusCode.OK);
+	        var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", false)
+		        .AddJsonFile("appsettings.secret.json", false)
+		        .Build();
+
+	        var credentials = configuration.Get<IdentiyCredentials>();
+	        var server = configuration.Get<IdentityServer>();
+
+			var response = TestCredentials(server, credentials);
+			Console.WriteLine($"Result: {response.Result.HttpStatusCode == HttpStatusCode.OK}.");
+			Console.WriteLine("Press <Enter> to exit.");
 	        Console.ReadLine();
         }
 
-	    static async Task<TokenResponse> TestCredentials(string user, string pwd)
+	    /// <summary>	Tests credentials. </summary>
+	    /// <param name="server">	  	The user. </param>
+	    /// <param name="credentials">	The password. </param>
+	    /// <returns>	A Task&lt;TokenResponse&gt; </returns>
+	    static async Task<TokenResponse> TestCredentials(IdentityServer server, IdentiyCredentials credentials)
 	    {
-			var disco = await DiscoveryClient.GetAsync("http://localhost:1234");
-		    var tokenClient = new TokenClient(disco.TokenEndpoint, "Jarvis", "Test");
-		    return await tokenClient.RequestResourceOwnerPasswordAsync("a.schnell@wtschnell.de", pwd, "api1");
+			Console.WriteLine($"Testing server '{server.TargetServer}'...");
+			Console.WriteLine($"ClientId: '{credentials.ClientId}'.");
+			Console.WriteLine($"UserName: '{credentials.UserName}'.");
+
+			var disco = await DiscoveryClient.GetAsync(server.TargetServer);
+		    var tokenClient = new TokenClient(disco.TokenEndpoint, credentials.ClientId, credentials.ClientSecret);
+		    return await tokenClient.RequestResourceOwnerPasswordAsync(credentials.UserName, credentials.Password, server.Api);
 	    }
     }
 }
