35d945c42f93541e177fa2037c8851b3c0aaa5f2
SAPPHIRE\aschnell <a.schnell@wtschnell.de>
2017-04-27 20:15:59 +0200

    Added some UnitTests

---
+++ src/FluiTec.Vision.Server.Data.Mssql.Tests/DataServiceHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace FluiTec.Vision.Server.Data.Mssql.Tests
+{
+    /// <summary>	A data service helper. </summary>
+    public static class DataServiceHelper
+    {
+	    /// <summary>	Gets the get. </summary>
+	    /// <returns>	A VisionDataService. </returns>
+	    public static VisionDataService Get()
+	    {
+		    return new VisionDataService(new LoggerFactory(), new TestDataSettings());
+	    }
+
+	    private class TestDataSettings : IDataServiceSettings
+	    {
+		    public string DefaultConnectionString => @"Data Source=.\SQLEXPRESS;Initial Catalog=Vision;Integrated Security=True";
+	    }
+    }
+}
