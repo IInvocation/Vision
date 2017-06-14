2c8a473db0bd1000e9519156b6c023ce232192c0
Achim Schnell <a.schnell@wtschnell.de>
2017-06-14 16:08:08 +0200

    Added several hosts to test custom store implementations

---
+++ src/FluiTec.Vision.Server.Host.AspCoreHost/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluiTec.Vision.Server.Host.AspCoreHost.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
---
+++ src/FluiTec.Vision.Server.Host.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluiTec.Vision.Server.Host.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
