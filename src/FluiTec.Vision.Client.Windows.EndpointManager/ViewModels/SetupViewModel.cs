9d18b0b7c16575954a7c1f6f62e724e6e7ac6150
Achim Schnell <a.schnell@wtschnell.de>
2017-07-13 17:39:05 +0200

    Added FinishCommand

--- src/FluiTec.Vision.Client.Windows.EndpointManager/ViewModels/SetupViewModel.cs
+++ src/FluiTec.Vision.Client.Windows.EndpointManager/ViewModels/SetupViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
 using FluiTec.Vision.Client.Windows.EndpointManager.Resources.Localization;
 using FluiTec.Vision.Client.Windows.EndpointManager.ViewModels.SetupWizard;
 using FluiTec.Vision.Client.Windows.EndpointManager.ViewModels.Wizard;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 
 namespace FluiTec.Vision.Client.Windows.EndpointManager.ViewModels
 {
@@ -12,7 +15,16 @@ namespace FluiTec.Vision.Client.Windows.EndpointManager.ViewModels
 		/// <summary>	Default constructor. </summary>
 		public SetupViewModel()
 		{
+			// init view content
 			Title = Global.ApplicationName;
+
+			// init commands
+			FinishCommand = new RelayCommand(() =>
+			{
+				MessageBox.Show(messageBoxText: "Finish");
+			});
+
+			// init wizard
 			Wizard = new WizardModel
 			{
 				Pages = new List<WizardPageViewModel>
@@ -29,5 +41,9 @@ namespace FluiTec.Vision.Client.Windows.EndpointManager.ViewModels
 		/// <summary>	Gets or sets the wizard. </summary>
 		/// <value>	The wizard. </value>
 		public WizardModel Wizard { get; set; }
+
+		/// <summary>	Gets or sets the finish command. </summary>
+		/// <value>	The finish command. </value>
+		public ICommand FinishCommand { get; set; }
 	}
 }
