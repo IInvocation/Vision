323f95fa13c99cace15f5600045f19ea6eded18b
Achim Schnell <a.schnell@wtschnell.de>
2017-08-04 10:31:45 +0200

    Enabled browser-cache for static files

---
+++ src/FluiTec.Vision.Server.Host.AspCoreHost/Configuration/StaticFileOptions.cs
@@ -0,0 +1,13 @@
+﻿using FluiTec.AppFx.Options;
+
+namespace FluiTec.Vision.Server.Host.AspCoreHost.Configuration
+{
+	/// <summary>	Static file options. </summary>
+	[ConfigurationName(name: "StaticFiles")]
+	public class StaticFileOptions
+	{
+		/// <summary>	Gets or sets the duration of the cache. </summary>
+		/// <value>	The cache duration. </value>
+		public int CacheDuration { get; set; }
+	}
+}
---
+++ src/FluiTec.Vision.Server.Host.AspCoreHost/StartUpExtensions/StaticFileExtension.cs
@@ -0,0 +1,31 @@
+﻿using FluiTec.AppFx.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
+
+namespace FluiTec.Vision.Server.Host.AspCoreHost.StartUpExtensions
+{
+	/// <summary>	A static file extension. </summary>
+	public static class StaticFileExtension
+	{
+
+		/// <summary>	An IApplicationBuilder extension method that use static files. </summary>
+		/// <param name="app">				The app to act on. </param>
+		/// <param name="configuration">	The configuration. </param>
+		/// <returns>	An IApplicationBuilder. </returns>
+		public static IApplicationBuilder UseStaticFiles(this IApplicationBuilder app, IConfigurationRoot configuration)
+		{
+			var cacheDuration = configuration.GetConfiguration<Configuration.StaticFileOptions>().CacheDuration;
+			app.UseStaticFiles(new StaticFileOptions
+			{
+				OnPrepareResponse = ctx =>
+				{
+					ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+						"public,max-age=" + cacheDuration;
+				}
+			});
+
+			return app;
+		}
+	}
+}
